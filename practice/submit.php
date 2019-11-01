54c5f868b2e0476d58e59b17d2bb001d65d4c839
Tyler Lucas <tjl25@zips.uakron.edu>
2019-11-01 16:06:56 -0400

    Update submit.php

--- practice/submit.php
+++ practice/submit.php
@@ -1,63 +1,18 @@
 <?php
-include 'db_connection.php';
-
-$conn = OpenCon();
-
-echo "Connected Successfully";
+	require "header.php";
 ?>
 
-<!doctype html>
-<html>
-    <head>
-        <?php
-        if(isset($_POST['upload']))
-        {
-            $maxsize = 20971520; // 20mb limit
-            
-            $name = $_FILES['file']['name'];
-            $target_dir = "videos/";
-            $target_file = $target_dir . $_FILES["file"]["name"];
-            $description = $_POST['descript'];
+	<main>
+		<?php
+			if (isset($_SESSION['username'])){
+				echo '<p> You are logged in </p>';
+			}
+			else {
+				echo '<p> You are logged out </p>';
+			}
+		?>
+	</main>
 
-            // Select file type
-       	    $videoFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-            // Valid file extensions
-            $extensions_arr = array("mp4","avi","3gp","mov","mpeg");
-            
-            if(in_array($videoFileType, $extensions_arr))
-            {
-		if(($_FILES['file']['size'] >= $maxsize) || ($_FILES["file"]["size"] == 0))
-		{
-			echo "File too large. File must be less than 5MB.";
-                }
-                else
-                {
-          		if(move_uploaded_file($_FILES['file']['tmp_name'],$target_file))
-			{
-				$query = "INSERT INTO Videos(Filename,Fileloc,FileDescript) VALUES('".$name."','".$target_file."','".$description."')";
-
-              			mysqli_query($conn,$query);
-              			echo "Upload successfully.";
-                        }
-                }
-            } 
-            else
-            {
-             echo "Invalid Extension";
-            }
-        }
-        ?>
-   </head>
-   <body>
-    <form method="post" action="" enctype='multipart/form-data'>
-      <input type='file' name='file' />
-      <input type='text' name='descript' />
-      <input type='submit' value='Upload' name='upload'>
-    </form>
-
-  </body>
-</html>
-            			
-        
-
+<?php
+	require "footer.php";
+?>
